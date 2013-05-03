7684d29e3e41bf836af61f493191934d1ff96998
Brandon Thomas <bt@brand.io>
2013-05-03 16:39:48 -0400

    Start of use of Youtube API.... Very messy

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 
 var main = function() {
 	var END = 200;
+
 	$(window).scroll(function(ev) {
 		var s = $(window).scrollTop();
 		//var t = $('#photo p');
@@ -19,4 +20,58 @@ var main = function() {
 		//t.css('margin-left', perc*100);
 		//console.log(perc);
 	});
+
+	installVideo();
 }
+
+var installVideo = function()
+{
+	var $script = $('<script src="https://www.youtube.com/iframe_api"></script>');
+	$('#photo').append($script);
+}
+
+player = null;
+
+function onYouTubeIframeAPIReady() {
+	// Note: 'player' is the div id to attach to
+	$('#photo').on('click', install2);
+}
+
+var install2 = function() {
+	player = new YT.Player('player', {
+		width: '1',
+		height: '1',
+		videoId: 'WR3Focm44ck',
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+
+var attached = false; 
+var onPlayerReady = function(ev) {
+	if(attached) {
+		ev.target.playVideo();
+		return;
+	}
+	attached = true;
+
+	console.log('onplayerready');
+	var $player = $('#player'),
+		$photo = $('#photo');
+
+	$photo.html($player);
+	$player.show();
+	$photo.show();
+
+	$player.width($photo.width());
+	$player.height($photo.height());
+
+	ev.target.playVideo();
+}
+
+var onPlayerStateChange = function() {
+}
+
