8e6fbf28ea5e1d92ab4460fda1aa3bf25eca0c3f
Brandon Thomas <bt@brand.io>
2013-06-03 13:25:17 -0400

    Improve upload script

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-rsync -zrpv --exclude-from=exclude.txt . isiglobal@isimobile.com:/home/isiglobal/isi.isimobile.com/flaskapp
+rsync -zrpvu --exclude-from=exclude.txt --progress . isiglobal@isimobile.com:/home/isiglobal/isi.isimobile.com/flaskapp
