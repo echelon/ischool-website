caa383a26fb112896ece24156560357b2f0c3324
Brandon Thomas <bt@brand.io>
2013-06-03 12:09:43 -0400

    Serve images with dynamic filters (WIP)

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ TODO: Static site compile tool
 
 import sys
 import os
-from flask import Flask, render_template, url_for
-from flask import send_from_directory
+import Image, ImageFilter # PIL
+from flask import Flask, render_template, url_for, request
+from flask import send_from_directory, send_file
+from StringIO import StringIO
 
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+FLASK_PATH = os.path.dirname(os.path.abspath(__file__))
 
 """
 APPLICATION COMPONENTS
@@ -54,6 +58,26 @@ def page_html_file(filename):
 			os.path.join(app.root_path, 'static', 'pages'),
                                filename)
 
+@app.route('/filter')
+def page_image_filter():
+	def get_fullpath(filename):
+		if '..' in filename:
+			return False
+		elif './' in filename:
+			return False
+		if filename.startswith('/'):
+			filename = filename[1:]
+		return os.path.join(FLASK_PATH, filename)
+
+	fn = get_fullpath(request.args['file'])
+	im = Image.open(fn)
+
+	if 'gaussian' in request.args:
+		kern = int(request.args['gaussian'])
+		im = im.filter(Gaussian(kern))
+
+	return serve_pil_image(im)
+
 @app.errorhandler(404)
 def page_404(e):
 	return render_template('404.html'), 404
@@ -64,6 +88,28 @@ TODO: Later
 def page_booking():
 	return render_template('booking.html')
 """
+
+
+def serve_pil_image(pil_img, quality=90):
+	"""
+	Serve a PIL image to the browser.
+	From: http://stackoverflow.com/a/10170635
+	"""
+	print 'one'
+	img_io = StringIO()
+	print 'two'
+	pil_img.save(img_io, 'JPEG', quality=quality)
+	print 'three'
+	img_io.seek(0)
+	print 'four'
+	return send_file(img_io, mimetype='image/jpeg')
+
+class Gaussian(ImageFilter.Filter):
+	name = "GaussianBlur"
+	def __init__(self, radius=2):
+		self.radius = radius
+	def filter(self, image):
+		return image.gaussian_blur(self.radius)
 
 def cache_buster():
 	import random
@@ -76,6 +122,7 @@ if __name__ == '__main__':
 			else int(sys.argv[1])
 	app.run(
 		port = port,
-		host = '0.0.0.0'
+		host = '0.0.0.0',
+		use_reloader = True,
 	)
 
