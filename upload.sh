6d47cccec98a4f168163ba1523eca4ff74455344
Brandon Thomas <bt@brand.io>
2013-06-10 12:54:19 -0400

    Fix htaccess and build script

--- upload.sh
+++ upload.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 
 echo 'Compiling LESS...'
-lessc --yui-compress static/less/design.less > design.out.css
+lessc --yui-compress static/less/design.less > static/less/design.out.css
+echo 'Running build...'
+python build.py
 
-echo 'Running rsync to development...'
-rsync -zrpvu -e ssh --exclude-from=exclude.txt --progress . isiglobal@isimobile.com:/home/isiglobal/isi.isimobile.com/flaskapp
+#echo 'Running rsync to development...'
+#rsync -zrpvu -e ssh --exclude-from=exclude.txt --progress . isiglobal@isimobile.com:/home/isiglobal/isi.isimobile.com/flaskapp
 
 echo 'Running rsync to staging...'
-rsync -zrpvu -e ssh --exclude-from=exclude.txt --progress ./output_build isiglobal@isimobile.com:/home/isiglobal/staging.isimobile.com
-
+rsync -zrpvu -e ssh --exclude-from=exclude.txt --progress ./output_build/ isiglobal@isimobile.com:/home/isiglobal/staging.isimobile.com
+rsync -zrpvu -e ssh --exclude-from=exclude.txt --progress ./static isiglobal@isimobile.com:/home/isiglobal/staging.isimobile.com
