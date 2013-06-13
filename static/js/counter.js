5e724198accd826b3bffb60e2b006dd54ee342b7
Brandon Thomas <bt@brand.io>
2013-06-13 08:17:25 -0400

    Design consistency, fonts, margins, etc.

--- static/js/counter.js
+++ static/js/counter.js
@@ -117,7 +117,8 @@ var StatView = Backbone.View.extend({
 		var num = format(this.model.getNumber()),
 			desc = this.model.get('description');
 
-		this.$el.html('<p class="number">' + num + '</p><p>' + desc + '</p>');
+		this.$el.html('<p class="number">' + num + '</p><p>' 
+						+ desc + '</p>');
 	},
 });
 
