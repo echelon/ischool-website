28877cfc8095d4b93bf4e524022e7bccfe20210f
Brandon Thomas <bt@brand.io>
2013-06-23 14:27:00 -0400

    Temp google form on pd page

--- static/js/form_tour.js
+++ static/js/form_tour.js
@@ -11,3 +11,11 @@ var install_form_tour = function() {
 		window.location = TOUR_FORM_URL;
 	});
 }
+
+// For now, all we're doing is sending them to the Google Docs form
+// In time, I'll write an inline, dynamic, and animated form. 
+var install_form_tour_on_pd = function() {
+	$('#book button').click(function() {
+		window.location = TOUR_FORM_URL;
+	});
+}
